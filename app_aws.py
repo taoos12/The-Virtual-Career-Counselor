c90fb3ebc29d06c694521aff24cc2373550e2f24
Sayed Mohammad Taoos <taoos0001@gmail.com>
2026-02-10 19:11:15 +0530

    Update app_aws.py

--- app_aws.py
+++ app_aws.py
@@ -23,7 +23,7 @@ projects_table = dynamodb.Table('Projects')
 enrollments_table = dynamodb.Table('Enrollments')
 
 # SNS Topic ARN
-SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:941377144321:aws_topic' 
+SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:713881794827:AWS_topic' 
 
 # File Upload Config
 UPLOAD_FOLDER = 'static/uploads'
